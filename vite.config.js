6e58f8b7497305a0e3ec3b2ba6568181a45ae794
Tan Vo <tanvo7411@gmail.com>
2024-03-18 17:26:15 -0700

    Set up Github

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
+    build: {
+        outDir: "./build",
+    },
+    base: "/UCLAX-Web1-Tan-Vo/",
 });
